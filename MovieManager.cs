914e5081627d2972a941bb66acc947b01af487a9
VickyBhaskar <vicky2400kumar@gmail.com>
2024-01-11 15:58:54 +0530

    initial Commit

---
+++ MovieManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesConApp
+{
+    public class MovieManager
+    {
+        private List<Movie> movies;
+
+        public MovieManager()
+        {
+            movies = new List<Movie>
+        {
+            new Movie { Title = "Movie 1", Director = "Director 1", ReleaseDate = DateTime.Parse("2010-04-11") },
+            new Movie { Title = "Movie 2", Director = "Director 2", ReleaseDate = DateTime.Parse("2012-02-12") },
+            new Movie { Title = "Movie 3", Director = "Director 3", ReleaseDate = DateTime.Parse("2011-03-10") },
+            new Movie { Title = "Movie 4", Director = "Director 4", ReleaseDate = DateTime.Parse("2013-05-15") }
+
+        };
+        }
+
+        public List<Movie> GetAllMovies()
+        {
+            return movies;
+        }
+
+        public Movie GetMovieByTitle(string title)
+        {
+            return movies.Find(m => m.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MoviesConApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            MovieManager movieManager = new MovieManager();
+
+            // Test GetAllMovies method
+            Console.WriteLine("All Movies:");
+            foreach (var movie in movieManager.GetAllMovies())
+            {
+                Console.WriteLine($"{movie.Title} - {movie.Director} - {movie.ReleaseDate.ToShortDateString()}");
+            }
+
+            // Test GetMovieByTitle method
+            string searchTitle = "Movie 1";
+            Movie foundMovie = movieManager.GetMovieByTitle(searchTitle);
+
+            if (foundMovie != null)
+            {
+                Console.WriteLine($"\nMovie found by title '{searchTitle}': {foundMovie.Title} - {foundMovie.Director} - {foundMovie.ReleaseDate.ToShortDateString()}");
+            }
+            else
+            {
+                Console.WriteLine($"\nMovie with title '{searchTitle}' not found.");
+            }
+        }
+    }
+}
+
